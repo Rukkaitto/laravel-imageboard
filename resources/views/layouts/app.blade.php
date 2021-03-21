26b8312732adef41589e4a170bb1ebf907813e5b
Lucas Goudin <goudinlucas@gmail.com>
2021-03-21 11:19:05 +0100

    Changed asset to secure_asset for css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
         <title>@yield('title')</title>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         <div class="flex flex-row space-x-16">
